57388e6ebdbdeaa8286dc5db7a47a040345b87fd
Sadra Mesbah <sadra.mesbah@gmail.com>
2024-11-12 14:25:23 -0500

    Add constructors, getters, and setters for Car class

--- src/main/java/com/sadramesbah/cars_inventory_api/entity/Car.java
+++ src/main/java/com/sadramesbah/cars_inventory_api/entity/Car.java
@@ -31,4 +31,85 @@ public class Car {
 
     @Column(precision = 10, scale = 2, nullable = false)
     private double price;
+
+
+    // Default constructor
+    public Car() {}
+
+    // Parameterized constructor
+    public Car(Long id, String vin, String make, String model, int year, String color, int mileage, double price) {
+        this.id = id;
+        this.vin = vin;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.mileage = mileage;
+        this.price = price;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
