1bee48d04ba2022ed66dfd509a31bf3e38a6712c
Sadra Mesbah <sadra.mesbah@gmail.com>
2024-11-12 12:25:56 -0500

    Create spring boot application

---
+++ src/main/java/com/sadramesbah/cars_inventory_api/CarDealershipInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sadramesbah.cars_inventory_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealershipInventoryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sadramesbah/cars_inventory_api/CarDealershipInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadramesbah.cars_inventory_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDealershipInventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
