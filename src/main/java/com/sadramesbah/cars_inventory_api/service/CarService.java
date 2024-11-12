fd3b44b35d0a8081ff4a48b462dbd883020d07a5
Sadra Mesbah <sadra.mesbah@gmail.com>
2024-11-12 15:55:16 -0500

    Add save method to CarService class

--- src/main/java/com/sadramesbah/cars_inventory_api/service/CarService.java
+++ src/main/java/com/sadramesbah/cars_inventory_api/service/CarService.java
@@ -1,13 +1,28 @@
 package com.sadramesbah.cars_inventory_api.service;
 
 import com.sadramesbah.cars_inventory_api.repository.CarRepository;
+import com.sadramesbah.cars_inventory_api.entity.Car;
 import org.springframework.stereotype.Service;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 @Service
 public class CarService {
+    private static final Logger logger = LoggerFactory.getLogger(CarService.class);
     private final CarRepository carRepository;
 
     public CarService(CarRepository carRepository) {
         this.carRepository = carRepository;
+    }
+
+    public Car saveCar(Car car) {
+        try {
+            return carRepository.save(car);
+
+        } catch (Exception e) {
+            logger.error("Failed to save car: {}", e.getMessage());
+            throw new RuntimeException("Failed to save car: " + e.getMessage());
+        }
     }
 }
